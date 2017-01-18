cebd6708a1dda4df2a5c61e55c7b6a21ab568d78
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-01-18 22:28:57 +0530

    Add char order

---
+++ char-order.py
@@ -0,0 +1,30 @@
+def inAscOrder(text):
+	for i in range(len(text)-2):
+		if ord(text[i])>ord(text[i+1]):
+			return False
+	return True
+	
+def inDescOrder(text):
+	for i in range(len(text)-2):
+		if ord(text[i])<ord(text[i+1]):
+			return False
+	return True
+	
+def allAlpha(s):
+	for c in s:
+		if not c.isalpha():
+			return False
+	return True
+	
+text = raw_input("Enter text: ")
+
+if allAlpha(text):
+	if inAscOrder(text.upper()):
+		print text+" IN ORDER"
+	elif inDescOrder(text.upper()):
+		print text+" REVERSE ORDER"
+	else:
+		print text+" NOT IN ORDER"
+else:
+	print "ERROR"
+	
