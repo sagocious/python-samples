313e9a6fe82d9a907f63371e6ffbca5c04496007
WeerakoonOS <oswucsc@gmail.com>
2018-10-03 11:42:07 +0530

    find factorial of a number

--- find_factorial.py
+++ find_factorial.py
@@ -1,6 +1,9 @@
-def F(n):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: return F(n-1)+F(n-2)
+def Factorial(n):
+    if n == 0: 
+		return 0
+    elif n == 1: 
+		return 1
+    else: 
+		return Factorial(n-1)+Factorial(n-2)
 
-F(3):
+Factorial(3):
